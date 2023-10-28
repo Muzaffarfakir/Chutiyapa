ac5f6b324b5c3230a01e03334fa1f1e306c5e1a1
Fakir Muzaffar Nisar <98309599+Muzaffarfakir@users.noreply.github.com>
2023-10-28 13:10:10 +0530

    Update main.js

--- build/main.js
+++ build/main.js
@@ -1,6 +1,6 @@
 let d = document.getElementById("d");
 window.addEventListener("load", (e) => {
-  for (let i = 0; i < 100; i++) {
+  for (let i = 0; i < 40; i++) {
     d.innerHTML += `  <frameset rows="${i}%">
       <iframe src="https://adsweb-6zlx.onrender.com/"></iframe>
   
